ba7fdeeaff6c1ea97ed085f250562c7401591a7f
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-04 17:17:23 -0300

    Update boolean.js

--- Fundamentos/boolean.js
+++ Fundamentos/boolean.js
@@ -2,3 +2,16 @@ let isActive = false;
 console.log(isActive);
 isActive = 1;
 console.log(!!isActive);
+
+//true
+console.log(!!' ');
+console.log(!![]);
+console.log(!!{});
+console.log(!!Infinity);
+console.log(!!(isActive = true));
+//false
+console.log(!!NaN);
+console.log(!!undefined);
+console.log(!!0)
+//retorna o primeiro true
+console.log('' || 0 || false || NaN || "oi");
