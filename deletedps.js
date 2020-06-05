f9a9946c2992803d4c4eb8df29cd72febf8682cc
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-05 01:45:17 -0300

    teste js

---
+++ deletedps.js
@@ -0,0 +1,11 @@
+function qualquerCoisa(numero) {
+    if (numero > 14 && numero < 18) {
+        console.log("aprendeu nao miseravi");
+    } else if (numero >= 18) {
+        console.log("aprendeu com don fernando!!");
+    }
+    else {
+        console.log("prossiga");
+    }
+}
+qualquerCoisa(20);
---
+++ objects.js
@@ -0,0 +1,13 @@
+//caracteristicas de objetos
+const prodct01 = {}
+prodct01.nome = "Notebook Lenovo Thinkpad T430";
+prodct01.preco = 2200.00;
+
+console.log(prodct01);
+
+//objeto literal
+const prodct02 = {
+    nome : "Notebook Dell Vostro",
+    preco: 3499.00
+}
+console.log(prodct02);
