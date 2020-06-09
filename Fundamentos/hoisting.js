88f3ba75b1ea7bc6a23a8e23115b5f1a22282d48
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-09 16:37:38 -0300

    Create hoisting.js

---
+++ Fundamentos/hoisting.js
@@ -0,0 +1,13 @@
+console.log('a = ', a);
+var a = 3;
+console.log('a =', a);
+
+//içamento de hoisting em var
+
+
+
+//console.log('b = ', b);
+//let b = 6;
+//console.log('b =', b);
+
+//Let não tem hoisting
