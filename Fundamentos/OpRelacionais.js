931eb5e526b109fcf75d6c271a5cce525af8dbdb
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-14 19:50:42 -0300

    Update OpRelacionais.js

--- Fundamentos/OpRelacionais.js
+++ Fundamentos/OpRelacionais.js
@@ -7,10 +7,13 @@ console.log("5)", 3 > 2);
 console.log("6)", 3 < 2);
 console.log("7)", 3 >= 2);
 console.log("8)", 3 <= 2);
-console.log("9)",);
-console.log("10)",);
 
-console.log("11)",);
+const data01 = new Date(0);
+const data02 = new Date(0);
+console.log("9)", data01 == data02);
+
+console.log("10)", data01.getTime() == data02.getTime());
+console.log("11)", undefined == null);
 console.log("12)",);
 console.log("13)",);
 console.log("14)",);
