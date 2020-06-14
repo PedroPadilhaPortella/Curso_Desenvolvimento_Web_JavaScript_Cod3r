76ee98f517aa9694fc7fa6f1550810f9eb358c57
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-14 19:48:18 -0300

    operador relacional

---
+++ Fundamentos/OpRelacionais.js
@@ -0,0 +1,16 @@
+console.log("1)", '1' == 1);
+console.log("2)", '1' === 1);
+console.log("3)", '3' != 3);
+console.log("4)", '3' !== 3);
+
+console.log("5)", 3 > 2);
+console.log("6)", 3 < 2);
+console.log("7)", 3 >= 2);
+console.log("8)", 3 <= 2);
+console.log("9)",);
+console.log("10)",);
+
+console.log("11)",);
+console.log("12)",);
+console.log("13)",);
+console.log("14)",);
