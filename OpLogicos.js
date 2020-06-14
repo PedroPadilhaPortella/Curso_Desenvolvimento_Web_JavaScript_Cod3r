cf3a05357f9c5a6ab9bd1743686c36c208729ce4
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-14 20:04:51 -0300

    Update OpLogicos.js

--- OpLogicos.js
+++ OpLogicos.js
@@ -1,5 +1,20 @@
 function compras (trabalho01, trabalho02) {
     const comprarSorvete = trabalho01 || trabalho02
     const comprarTv50 = trabalho01 && trabalho02
-    const comprarTv32 = 
+    //const comprarTv32 = !!(trabalho01 ^ trabalho02)/bitwise xor
+    const comprarTv32 = trabalho01 != trabalho02
+    const manterSaudavel = !comprarSorvete
+
+    //retorno de objetos com valor implicito
+    return {
+        comprarSorvete,
+        comprarTv32,
+        comprarTv50,
+        manterSaudavel
+    }
 }
+
+console.log(compras(true, true))
+console.log(compras(true, false))
+console.log(compras(false, true))
+console.log(compras(false, false))
