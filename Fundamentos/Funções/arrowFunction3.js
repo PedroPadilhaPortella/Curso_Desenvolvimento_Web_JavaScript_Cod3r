3af10233ea7e883a21b77515129817281c0747b4
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-28 02:04:48 -0300

    Update arrowFunction3.js

--- Fundamentos/Funções/arrowFunction3.js
+++ Fundamentos/Funções/arrowFunction3.js
@@ -2,4 +2,10 @@ let compararThis = function (param){
     console.log(this == param)
 }
 
-compararThis(global)
+compararThis(global) //this aponta para global
+
+
+const obj = {}
+compararThis = compararThis.bind(obj) //alteramos o bind do this para objeto
+compararThis(global) //aqui this não aponta mais para global
+compararThis(obj) // e aponta para o objeto
