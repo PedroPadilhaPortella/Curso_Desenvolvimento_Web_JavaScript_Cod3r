4d15301e17da46f249722cdbf6a709086cda32e2
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-30 16:56:47 -0300

    Update Closure.js

--- JavaScript/Funções/Closure.js
+++ JavaScript/Funções/Closure.js
@@ -2,3 +2,16 @@
 Closure é o escopo criado quando uma função é declarada,
 esse escopo permite a função acessar e manipular variáveis externas à função
 */
+
+const valor = 'Global'
+
+function fora (){
+    const valor = 'Local'
+    function dentro (){
+        return valor
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
