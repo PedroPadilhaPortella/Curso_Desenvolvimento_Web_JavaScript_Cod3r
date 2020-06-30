22583c1d82664461fb970836e8e364aa7a249d0d
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-06-30 16:36:46 -0300

    Update FunçãoConstrutora.js

--- JavaScript/Funções/FunçãoConstrutora.js
+++ JavaScript/Funções/FunçãoConstrutora.js
@@ -1,11 +1,11 @@
-function Carro(delta = 5, velocidadeMaxima = 200){
+function Carro( velocidadeMaxima = 200, delta = 5){
     //atributo privado
     let velocidadeAtual = 0
 
     //metodo publico
     this.acelerar = function(){
         if(velocidadeAtual + delta <= velocidadeMaxima){
-                velocidadeAtual += delta
+            velocidadeAtual += delta
         }else{
             velocidadeAtual = velocidadeMaxima
         }
@@ -15,5 +15,18 @@ function Carro(delta = 5, velocidadeMaxima = 200){
     this.getVelocidadeAtual = function (){
         return velocidadeAtual
     }
-
 }
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+
+console.log(typeof Carro, typeof ferrari, typeof uno)
