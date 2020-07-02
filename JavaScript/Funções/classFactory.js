1b42e68c28b2950c49efbf4377d1f5fb8a0b6a81
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-02 00:30:51 -0300

    factory and class

--- JavaScript/Funções/classFactory.js
+++ JavaScript/Funções/classFactory.js
@@ -1,3 +1,4 @@
+//class Pessoa
 class Pessoa{
     constructor(nome){
         this.nome = nome;
@@ -7,5 +8,27 @@ class Pessoa{
     }
 }
 
-const pedro = new Pessoa('Pedro')
-pedro.falar()
+const p0 = new Pessoa('Pedro')
+p0.falar()
+
+//function Factory Pessoa
+function Pessoa1(nome){
+    this.nome = nome
+    this.falar= function(){
+        console.log(`Meu nome é ${this.nome}`)
+
+    }
+}
+
+const p1 = new Pessoa1 ('Andre')
+p1.falar()
+
+//Factory usando arrow function
+const pessoa = nome => {
+    return{
+        falar2: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const joao = pessoa('Joao')
+joao.falar2()
