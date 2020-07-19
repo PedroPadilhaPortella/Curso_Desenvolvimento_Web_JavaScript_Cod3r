f5e2eb806de8f7bbcf89b2b751fce5a6ff9fc4b0
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-19 02:21:29 -0300

    Update herança06.js

--- JavaScript/Orientação a Objetos/herança06.js
+++ JavaScript/Orientação a Objetos/herança06.js
@@ -5,3 +5,17 @@ function Aula(nome, videoId){
 
 const aula1 = new Aula('Introdução ao Javascript', 01)
 const aula2 = new Aula('Operadores', 02)
+console.log(aula1, aula2)
+
+//Simulando operador NEW, pegando a função e os parametros dela e atribuido a um objeto construtor
+
+function novo(f, ...params){
+        const obj = {}
+        obj.__proto__ = f.prototype
+        f.apply(obj, params)
+        return obj
+}
+
+const aula3 = novo(Aula, 'Orientação a Objetos', 03)
+const aula4 = novo(Aula, 'Banco de dados', 04)
+console.log(aula3, aula4)
