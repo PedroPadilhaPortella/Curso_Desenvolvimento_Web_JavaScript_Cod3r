00784e6da5b767287c438e797f4d9074ab0964ff
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-20 21:36:08 -0300

    Create classe01.js

---
+++ JavaScript/Orientação a Objetos/classe01.js
@@ -0,0 +1,31 @@
+class Lancamento{
+    constructor(nome = "Anonimo", valor = 0){
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro{
+    constructor(ano, mes){
+        this.ano = ano
+        this.mes = mes
+        this.lancamentos = []
+    }
+    addLancamentos(...lancamentos){
+        lancamentos.forEach(lanca => this.lancamentos.push(lanca))
+    }
+    sumario(){
+        let valorConsolidado = 0
+        this.lancamentos.forEach(lanca => {
+            valorConsolidado += lanca.valor
+        })
+        return valorConsolidado
+    }
+}
+
+const salario = new Lancamento('salario', 2600);
+const contaLuz = new Lancamento('luz', -220)
+
+const contas = new CicloFinanceiro(2020, 08)
+contas.addLancamentos(salario, contaLuz)
+console.log(contas.sumario())
