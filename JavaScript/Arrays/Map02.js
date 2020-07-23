2a4acdae7a36428d66a15777ea95ed78711fb2c3
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-22 23:42:22 -0300

    Update Map02.js

---
+++ JavaScript/Arrays/Map02.js
@@ -0,0 +1,13 @@
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit Lápis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}'
+]
+
+const paraObjeto = json => JSON.parse(json);
+const getPrecos = array => array.preco;
+
+let precos = carrinho.map(paraObjeto).map(getPrecos)
+
+console.log(precos)
