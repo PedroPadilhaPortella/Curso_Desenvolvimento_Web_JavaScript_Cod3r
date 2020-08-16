657750b9dac3d2b7d3070dbbd8d76d07719606bb
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-08-16 02:11:41 -0300

    Update Promise_4.js

--- ESNext/Promises/Promise_4.js
+++ ESNext/Promises/Promise_4.js
@@ -12,14 +12,20 @@ function gerarAleatório(min, max, tempo) {
      })
 }
 
+//essa Promise irá chamar as 5 funções ao mesmo tempo e quando todas forem resolvidas, ele da o retorno e chama o then da função. No caso, ira resolver após 10 segundos, que é o maior tempo de requisição de promise.
 function gerarVariosNumeros () {
     return Promise.all([
         gerarAleatório(10, 100, 1000),
         gerarAleatório(10, 100, 10000),
         gerarAleatório(10, 100, 300),
         gerarAleatório(10, 100, 500),
-        gerarAleatório(10, 100, 2000),
+        gerarAleatório(10, 100, 10000),
     ])
 }
 
-gerarVariosNumeros().then(numeros => console.log(numeros))
+    console.time('tempo de execução')
+gerarVariosNumeros()
+    .then(console.log)
+    .then(() => {
+        console.timeEnd('tempo de execução')
+    })
