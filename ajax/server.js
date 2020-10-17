50998be4513fa68993595fa8dabe024d8226c63f
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-17 19:54:10 -0300

    xmlhttprequest

--- ajax/server.js
+++ ajax/server.js
@@ -6,6 +6,4 @@ app.use(express.static('.')) //esse middleware provê o servidor para os arquivo
 app.use(bodyParser.urlencoded({extended: true})) //middleware para converter os dados de formulários em objetos
 app.use(bodyParser.json()) //middleware para converter json para objetos
 
-app.get('/teste', (req, res) => res.send("OK"))
-
 app.listen('8080', () => console.log("Executando na porta 8080..."))
