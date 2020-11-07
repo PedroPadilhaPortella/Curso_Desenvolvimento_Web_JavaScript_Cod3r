76c2a7204e142cf348a8b58289865bdd60f81d84
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-11-06 23:18:51 -0300

    webpack

--- webpack/src/modulos/moduloB.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    saudacao() {return "Olá, eu sou o modulo B"}
-}
--- webpack/src/principal.js
+++
@@ -1,7 +0,0 @@
-// const Pessoa = require('./pessoa');
-import Pessoa from './pessoa';
-import './modulos/moduloA';
-import './assets/css/estilo.css';
-
-const atendente = new Pessoa
-console.log(atendente.cumprimentar())
