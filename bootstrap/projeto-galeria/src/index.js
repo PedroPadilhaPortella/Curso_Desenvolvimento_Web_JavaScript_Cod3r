ae3d98c0f98355932036fae6211662599c65a4f2
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-11-28 09:45:48 -0300

    projeto galeria bootstrap

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+//arquivo sass
+import './scss/index.scss'
+
+//Dependecias
+import 'jquery';
+import 'bootstrap';
+
+//Arquivos JS
+import './js/core/includes';
+import './js/plugins/cityButtons';
