335e54b13fd1960a7fafd7d3e8f305cbb4d338ab
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-12-22 20:50:14 -0300

    capitulo de VueJS
    calculadora, montyhall, exercicios e most-used-words

---
+++ vue/most-used-words/src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container fluid>
+      <div class="pills">
+          <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount"/>
+      </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from './Pill'
+
+export default {
+    components: { Pill },
+    data: function() {
+        return {
+            groupedWords: [
+                {name: 'you', amount: 122},
+                {name: 'increase', amount: 14},
+                {name: 'he', amount: 50},
+            ]
+        }
+    }
+}
+</script>
+
+
+<style>
+.pills {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+</style>
