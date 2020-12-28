f30df9b699eb43d674b8a95c89d2117ba25b645e
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-12-28 19:53:51 -0300

    comandos de bancos de dados relacionais mysql

--- banco-de-dados/banco-relacional/consultarDados.sql
+++
@@ -1,7 +0,0 @@
-select * from estados;
-
-select nome as 'Sigla do Estado', sigla from estados;
-
-select * from estados where regiao = 'Sudeste';
-
-select * from estados where populacao >= 10 order by populacao desc;
--- banco-de-dados/banco-relacional/criarTabelaEstados.sql
+++ banco-de-dados/banco-relacional/criarTabelaEstados.sql
@@ -1,10 +1,11 @@
-create table estados(
+-- Criando a tabela estado!
+create table estados (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL,
     sigla VARCHAR(2) NOT NULL,
-    regiao ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sul', 'Sudeste') NOT NULL,
+    regiao ENUM('Norte', 'Nordeste','Centro-Oeste', 'Sudeste', 'Sul') NOT NULL,
     populacao DECIMAL(5,2) NOT NULL,
-    PRIMARY KEY(id),
-    UNIQUE KEY(nome),
-    UNIQUE KEY(sigla)
+    PRIMARY KEY (id),
+    UNIQUE KEY (nome),
+    UNIQUE KEY (sigla)
 );
