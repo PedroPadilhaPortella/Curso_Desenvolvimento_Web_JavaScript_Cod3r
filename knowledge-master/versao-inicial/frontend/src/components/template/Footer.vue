1462f4df99b0ed7a21fe1b829bece6a41337f856
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-01-15 17:26:18 -0300

    configurando o frontend da aplicacao

---
+++ knowledge-master/versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+      <span>Copyright <strong>Pedro Portella</strong> &copy; 2021</span>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
--- knowledge-master/versao-inicial/frontend/src/main.js
+++ knowledge-master/versao-inicial/frontend/src/main.js
@@ -1,9 +1,12 @@
+import 'font-awesome/css/font-awesome.css';
 import Vue from 'vue'
 
 import App from './App'
+import store from './config/store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+    store,
+    render: h => h(App)
 }).$mount('#app')
